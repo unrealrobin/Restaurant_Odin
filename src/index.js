7c16bc5146fd3493b132fdb3f5427f77bb171ebf
Your NameRobin <rlifshitz19@gmail.com>
2020-01-23 17:34:23 -0700

    multifile working w/ webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+import { pageLoad, menuTab, contact } from './landing.js'
+
+pageLoad();
+
+
+//write tab switching logic
+//clears content of maincontainer
+//appends correct tabs functions
---
+++ src/landing.js
@@ -0,0 +1,20 @@
+//initial page load information here
+
+//module 1
+const pageLoad = () => {
+  const container = document.getElementById('content');
+  container.setAttribute('style', 'height: 300px; width: 300px; background-color: blue');
+}
+
+//module 1
+const menuTab = () => {
+
+}
+
+//module 3
+const contact = () => {
+
+}
+
+
+export { pageLoad, menuTab, contact };
