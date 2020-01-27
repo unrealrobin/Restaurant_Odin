2e6bba7307763df5debd0ce649c547adb8a483c5
Your NameRobin <rlifshitz19@gmail.com>
2020-01-26 20:48:13 -0700

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { background, menuTab, contact } from './landing.js'
+import { background, menuTab, contact } from './landing.js';
+
+let clearTab = () => {
+  while (tabContent.firstChild) {
+    tabContent.removeChild(tabContent.firstChild);
+  }
+}
 
 const tabContent = document.createElement('div');
 
@@ -6,26 +12,43 @@ let restaurant = () => {
   let contentContainer = document.getElementById('content');
   //creates tabs
   const tabBar = document.createElement('div');
+  tabBar.setAttribute('id', 'tabBar');
   const backgroundTab = document.createElement('div');
+  backgroundTab.textContent = "Background";
+  backgroundTab.addEventListener('click', function () {
+    background()
+  });
   const menu = document.createElement('div');
+  menu.textContent = "Menu";
+  menu.addEventListener('click', function () {
+    menuTab()
+  });
   const contactTab = document.createElement('div');
-  tabBar.appendChild(backgroundTab, menu, contactTab);
+  contactTab.textContent = "Contact";
+  contactTab.addEventListener('click', function () {
+    contact();
+  });
+  tabBar.appendChild(backgroundTab);
+  tabBar.appendChild(menu);
+  tabBar.appendChild(contactTab);
   //sets class and Id's
   let tabBarItems = Array.from(tabBar.childNodes);
-  for (let i = 0; i < tabBar.clientHeight; i++) {
+
+  for (let i = 0; i < tabBarItems.length; i++) {
     tabBarItems[i].setAttribute('class', 'allTabs');
     tabBarItems[i].setAttribute('id', 'tab' + i);
   }
   //contentContainer attaches tabBar as child
   contentContainer.appendChild(tabBar);
   //attaches the tabs content to page
+  tabContent.setAttribute('id', 'tabContent');
   contentContainer.appendChild(tabContent);
+};
 
 
-}
+restaurant();
 
-
-
+export { clearTab, tabContent };
 
 //write tab switching logic
 //clears content of maincontainer
--- src/landing.js
+++ src/landing.js
@@ -1,33 +1,30 @@
-//initial page load information here
-
-const clearTab = () => {
-  while (tabContent.firstChild) {
-    tabContent.removeChild(tabContent.firstChild);
-  }
-}
+import { clearTab, tabContent } from './index.js';
 
 //module 1
 const background = () => {
   clearTab();
   let backgroundContent = document.createElement('div');
   backgroundContent.textContent = "Here would be the background Information";
+  tabContent.appendChild(backgroundContent);
 }
 
 //module 2
 const menuTab = () => {
   clearTab();
-
   let menuContent = document.createElement('div');
   menuContent.textContent = "Here is the menu!";
+  tabContent.appendChild(menuContent);
+
 
 }
 
 //module 3
 const contact = () => {
   clearTab();
-
   let contactContent = document.createElement('div');
   contactContent.textContent = "Robin Lifshitz";
+  tabContent.appendChild(contactContent);
+
 
 }
 
